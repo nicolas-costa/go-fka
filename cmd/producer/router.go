9a46f70d7bcc4aec5697ff1069f82bd7f995e642
Nícolas Costa <nicolas.nico@hotmail.com>
2023-12-10 01:45:04 -0300

    feat: add health controller, service and route

---
+++ cmd/producer/router.go
@@ -0,0 +1,20 @@
+package producer
+
+import (
+	"gofka/internal/infrastructure/controllers"
+	"gofka/internal/infrastructure/servers"
+)
+
+type Router struct {
+	healthController *controllers.HealthController
+}
+
+func NewRouter(healthController *controllers.HealthController) *Router {
+	return &Router{
+		healthController: healthController,
+	}
+}
+
+func (r *Router) SetRoutes(app *servers.FiberServer) {
+	app.Get("/health", r.healthController.Check)
+}
---
+++ internal/infrastructure/controllers/health_controller.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"gofka/internal/application"
+)
+
+type HealthController struct {
+	healthService application.Checker
+}
+
+func NewHealthController(service application.Checker) *HealthController {
+	return &HealthController{
+		healthService: service,
+	}
+}
+
+func (h *HealthController) Check(c *fiber.Ctx) error {
+	return c.JSON(map[string]interface{}{
+		"healthy": h.healthService.Check(),
+	})
+}
