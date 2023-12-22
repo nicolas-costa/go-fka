b1a70cfd512ea03ac6d9ba40de1ca234f009cf2b
Nícolas Costa <nicolas.nico@hotmail.com>
2023-12-21 23:39:24 -0300

    feat: update logic to translate pings into pongs

--- internal/application/pong_service.go
+++ internal/application/pong_service.go
@@ -1,10 +1,13 @@
 package application
 
+import "strings"
+
 // Receiver is the output port
 type Receiver interface {
 	Receive() []string
 }
 
+// Ponger is the input port
 type Ponger interface {
 	Pong() []string
 }
@@ -17,6 +20,11 @@ func NewPongService(receiverRepository Receiver) *PongService {
 	return &PongService{repository: receiverRepository}
 }
 
-func (p *PongService) Pong() []string {
-	return p.repository.Receive()
+func (p *PongService) Pong() (pongs []string) {
+	for _, ping := range p.repository.Receive() {
+		pong := strings.Replace(ping, "i", "o", 1)
+		pongs = append(pongs, pong)
+	}
+
+	return pongs
 }
